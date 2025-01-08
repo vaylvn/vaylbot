bea86bb16e40649221a3111a0429a54ad7205cc9
vaylvn <164453259+vaylvn@users.noreply.github.com>
2025-01-08 05:32:57 +0000

    Minor Alert Fix
    Added redeem handler in alert management, removed in error.

--- vayl.py
+++ vayl.py
@@ -842,16 +842,22 @@ async def manageAlertsAsync():
                     if a["type"] == "giftsub" and a["gifter"] == alert["gifter"]:
                         alert["amount"] += 1
                         pop_amount += 1
-
-            with open(os.getcwd() + "\\configuration\\event\\" + alert["type"] + ".yml", 'r', encoding = "utf-8") as file:
-                data = yaml.safe_load(file)
-                if "enabled" in data and data["enabled"] == True:
-                    actions = data["actions"]
-                    buffer = data["buffer"]
-                    
+            
+            if "redeem" in alert["type"]:
+                actions = alert["actions"]
+                buffer = alert["buffer"]
+            else:
+                with open(os.getcwd() + "\\configuration\\event\\" + alert["type"] + ".yml", 'r', encoding = "utf-8") as file:
+                    data = yaml.safe_load(file)
+                    if "enabled" in data and data["enabled"] == True:
+                        actions = data["actions"]
+                        buffer = data["buffer"]
+                        
                      
                     
-            
+            await runActions(actions, alert)
+            for i in range(0, pop_amount):
+                sv["alerts"].pop(0)
             
             
             continue
